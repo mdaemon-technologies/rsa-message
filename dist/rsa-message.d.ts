851964cefa430c52c6249225111266867f1c0ada
MDaemon Technologies, Ltd. <jonathan.ehman@mdaemon.com>
2024-12-02 10:21:49 -0600

    Fixed the TS exports

--- dist/rsa-message.d.ts
+++ dist/rsa-message.d.ts
@@ -1,8 +1,20 @@
-declare class MDPGP {
-  init(): Promise<string>;
-  setPublicKey(userId: string, publicKey: string): void;
-  encryptMessage(message: string, userId: string): Promise<string>;
-  decryptMessage(encryptedMessage: string, userId: string): Promise<string>;
+interface IRSAEncryptedMessage {
+    iv: Uint8Array;
+    encryptedMessage: Uint8Array;
+    encryptedAESKey: Uint8Array;
+    signature: Uint8Array;
 }
-
-export default MDPGP;
+export { IRSAEncryptedMessage };
+export default class RSAMessage {
+    constructor();
+    get publickey(): string;
+    get privatekey(): string;
+    init(publicKey?: string, privateKey?: string): Promise<string>;
+    signMessage(message: string): Promise<ArrayBuffer>;
+    encryptMessage(message: string, userId: string): Promise<IRSAEncryptedMessage>;
+    verifySignature(signature: Uint8Array, message: string, userId: string): Promise<boolean>;
+    decryptMessage(encryptedData: IRSAEncryptedMessage, userId: string): Promise<string>;
+    setPublicKey(userId: string, publicKey: string): void;
+    exportEncryptedMessage(message: IRSAEncryptedMessage): string;
+    importEncryptedMessage(encoded: string): IRSAEncryptedMessage;
+}
